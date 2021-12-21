889a357b7b836a56e961f8d88cbf98f3874f9fcf
huffman442 <huffman442@yahoo.com>
2021-12-21 10:21:44 -0500

    implement material web +signup form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+require("jquery")
+require("packs/forms")
+
 
 Rails.start()
 Turbolinks.start()
